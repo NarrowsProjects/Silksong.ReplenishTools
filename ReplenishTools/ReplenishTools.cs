0454180af1487d44b7413d9d983b2bc3085644f0
Guust <metzguust@gmail.com>
2026-04-12 20:28:28 +0200

    Initialise project

---
+++ ReplenishTools/ReplenishTools.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Unity;
+using HarmonyLib;
+
+[BepInPlugin("com.NarrowsProjects.ReplenishTools", "Replenish tools over time", "1.0.0")]
+public class ReplenishTools : BaseUnityPlugin
+{
+    public void Awake()
+    {
+        Logger.LogInfo("Loading ReplenishTools mod");
+        Harmony.CreateAndPatchAll(typeof(ReplenishTools), null);
+    }
+}
