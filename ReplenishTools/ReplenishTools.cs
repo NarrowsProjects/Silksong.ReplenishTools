e9e8b5f6c82d613190c25c29dbb8c8cf6ed26b86
Guust <metzguust@gmail.com>
2026-04-19 15:58:37 +0200

    feat: attempt to half all red tool capacity

--- ReplenishTools/ReplenishTools.cs
+++ ReplenishTools/ReplenishTools.cs
@@ -2,6 +2,7 @@
 using BepInEx.Logging;
 using Unity;
 using HarmonyLib;
+using UnityEngine;
 
 [BepInPlugin("com.NarrowsProjects.ReplenishTools", "Replenish tools over time", "1.0.0")]
 public class ReplenishTools : BaseUnityPlugin
@@ -10,5 +11,15 @@ public class ReplenishTools : BaseUnityPlugin
     {
         Logger.LogInfo("Loading ReplenishTools mod");
         Harmony.CreateAndPatchAll(typeof(ReplenishTools), null);
+    }
+
+    [HarmonyPatch(typeof(ToolItem), nameof(ToolItem.BaseStorageAmount), MethodType.Getter)]
+    static class PatchToolCapacity
+    {
+        static void Postfix(ToolItem __instance, ref int __result)
+        {
+            if (__instance.Type == ToolItemType.Red)
+                __result = Mathf.Max(1, __result / 2);
+        }
     }
 }
